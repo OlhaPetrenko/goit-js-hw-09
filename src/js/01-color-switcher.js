00a02a1f782f665e7f10eb6dbc64590e6b51c5f7
OlhaPetrenko <o.l.petr27@gmail.com>
2022-07-13 21:13:50 +0300

    Додає завдання 1 ДЗ9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStartEl = document.querySelector('[data-start]');
+const btnStoptEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
+// console.log(bodyEl);
+
+btnStartEl.addEventListener('click', onBtnStartClick);
+function onBtnStartClick(event) {
+  event.target.setAttribute('disabled', true);
+  btnStoptEl.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    // console.log('Helo');
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+btnStoptEl.addEventListener('click', onBtnStopClick);
+
+function onBtnStopClick(event) {
+  event.target.setAttribute('disabled', true);
+  btnStartEl.removeAttribute('disabled');
+  clearInterval(timerId);
+  //   console.log(`Interval with id ${timerId} has stopped!`);
+}
