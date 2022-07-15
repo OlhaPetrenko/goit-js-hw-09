e9c7de0b26ec55f31d8fb711ffca219553505e51
OlhaPetrenko <104621174+OlhaPetrenko@users.noreply.github.com>
2022-07-15 11:34:47 +0000

    Deploying to gh-pages from @ OlhaPetrenko/goit-js-hw-09@999bc22c26028fc975de2a3c1d5ef9c6a63ebfd7 🚀

---
+++ 01-color-switcher.9f4356a4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(function(t){t.target.setAttribute("disabled",!0),e.removeAttribute("disabled"),a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(e){e.target.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.9f4356a4.js.map
